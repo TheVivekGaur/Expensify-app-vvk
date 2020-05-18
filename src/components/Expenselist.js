6474d2804a736bdea011af1aa5eb7e311e5e120c
TheVivekGaur <vvksngh24@gmail.com>
2020-05-19 00:27:54 +0530

    Add private routes

--- src/components/Expenselist.js
+++ src/components/Expenselist.js
@@ -5,10 +5,16 @@ import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
     <div>
-    <h1>ExpenseList</h1>
-    {props.expenses.map((expense)=>{
-  return <ExpenseListItem key = {expense.id} {...expense}/>
-    })}
+     {
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+          props.expenses.map((expense) => {
+            return <ExpenseListItem key={expense.id} {...expense} />;
+          })
+        )
+    }
+    
     </div>
 );
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,4 +15,6 @@ const  Config = {
 
   const database = firebase.database();
 
-  export { firebase , database as default };
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+  export { firebase , googleAuthProvider , database as default };
